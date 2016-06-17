490ba11b564a551ccaeef74a910992d9a4b8a37d
adelessafi <adelessafi@gmail.com>
2016-06-17 14:16:39 +0100

    layout change

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title')</title>
-    <base href="http://localhost:8000/">
+    <base href="http://www.mondocteur.ovh/">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
